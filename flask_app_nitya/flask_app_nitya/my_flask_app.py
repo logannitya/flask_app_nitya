17ee7c818de4870ab5c71bbc7e6b834c5e52da47
logannitya <nityaniranjansamal@gmail.com>
2020-11-18 11:45:08 +0000

    old_version

--- flask_app_nitya/flask_app_nitya/my_flask_app.py
+++ flask_app_nitya/flask_app_nitya/my_flask_app.py
@@ -29,7 +29,7 @@ books = [
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
+    return '''<h1>Distant Reading Archive old version</h1>
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
 
